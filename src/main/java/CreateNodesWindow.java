a140b0cf1e20edb695f7216263a4e4aed0f9af69
lolosuselessscripts <lovis@outlook.com>
2022-03-03 11:53:23 +0100

    Kleiner Fix

--- src/main/java/CreateNodesWindow.java
+++ src/main/java/CreateNodesWindow.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.Map;
 
 public class CreateNodesWindow {
     private JButton okButton;
